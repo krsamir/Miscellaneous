3ced41f7a0d82b83ccda38e791fee530052f25e2
Samir Kumar <46542096+krsamir@users.noreply.github.com>
2020-01-16 18:53:09 +0530

    Create Directory.py

---
+++ Directory.py
@@ -0,0 +1,13 @@
+#How to Get the current working directory ?
+
+import os
+'''
+    Get Current working Directory
+'''
+
+currentDirectory = os.getcwd()
+
+'''
+    Change the Current working Directory
+'''
+os.chdir('Some_other _location')
